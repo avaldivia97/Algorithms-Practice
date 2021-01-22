64948e78b2eea5220d2dbfc9ee52bfa58136d64a
avaldivia97 <avaldivia97@outlook.com>
2021-01-22 12:17:08 -0500

    Two problems for 01/22/2021

---
+++ 01222021/validSudoku.js
@@ -0,0 +1,68 @@
+// Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+
+// Each row must contain the digits 1-9 without repetition.
+// Each column must contain the digits 1-9 without repetition.
+// Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+// Note:
+
+// A Sudoku board (partially filled) could be valid but is not necessarily solvable.
+// Only the filled cells need to be validated according to the mentioned rules.
+ 
+
+// Example 1:
+
+
+// Input: board = 
+// [["5","3",".",".","7",".",".",".","."]
+// ,["6",".",".","1","9","5",".",".","."]
+// ,[".","9","8",".",".",".",".","6","."]
+// ,["8",".",".",".","6",".",".",".","3"]
+// ,["4",".",".","8",".","3",".",".","1"]
+// ,["7",".",".",".","2",".",".",".","6"]
+// ,[".","6",".",".",".",".","2","8","."]
+// ,[".",".",".","4","1","9",".",".","5"]
+// ,[".",".",".",".","8",".",".","7","9"]]
+// Output: true
+// Example 2:
+
+// Input: board = 
+// [["8","3",".",".","7",".",".",".","."]
+// ,["6",".",".","1","9","5",".",".","."]
+// ,[".","9","8",".",".",".",".","6","."]
+// ,["8",".",".",".","6",".",".",".","3"]
+// ,["4",".",".","8",".","3",".",".","1"]
+// ,["7",".",".",".","2",".",".",".","6"]
+// ,[".","6",".",".",".",".","2","8","."]
+// ,[".",".",".","4","1","9",".",".","5"]
+// ,[".",".",".",".","8",".",".","7","9"]]
+// Output: false
+// Explanation: Same as Example 1, except with the 5 in the top left corner being modified to 8. Since there are two 8's in the top left 3x3 sub-box, it is invalid.
+ 
+
+// Constraints:
+
+// board.length == 9
+// board[i].length == 9
+// board[i][j] is a digit or '.'.
+
+// Link to problem:
+// https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/769/
+
+var isValidSudoku = function(board) {
+    let row = {};
+    let col = {};
+    let boxs = {};
+    for(let i = 0; i < 9; i++){
+        for(let j = 0; j < 9; j++){
+            element = board[i][j];
+            if(element !== '.'){
+                let boxNum = Math.floor(i/3)*3 + Math.floor(j/3);
+                if(row[`${i}-${element}`] || col[`${j}-${element}`] || boxs[`${boxNum}-${element}`]) return false;
+                row[`${i}-${element}`] = true;
+                col[`${j}-${element}`] = true;
+                boxs[`${boxNum}-${element}`] = true;
+            }
+        }
+    }
+    return true;
+};
