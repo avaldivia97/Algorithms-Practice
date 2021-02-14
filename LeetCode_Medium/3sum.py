9461294e6b8cad87beb12f275e763618a80907ed
avaldivia97 <avaldivia97@outlook.com>
2021-02-14 17:37:27 -0500

    Two problems for 02/14/2021

---
+++ LeetCode_Medium/3sum.py
@@ -0,0 +1,46 @@
+# Given an array nums of n integers, are there elements a, b, c in nums such that a + b + c = 0? Find all unique triplets in the array which gives the sum of zero.
+# Notice that the solution set must not contain duplicate triplets.
+
+# Example 1:
+# Input: nums = [-1,0,1,2,-1,-4]
+# Output: [[-1,-1,2],[-1,0,1]]
+
+# Example 2:
+# Input: nums = []
+# Output: []
+
+# Example 3:
+# Input: nums = [0]
+# Output: []
+
+# Constraints:
+# 0 <= nums.length <= 3000
+# -10^5 <= nums[i] <= 10^5
+
+# Link to problem:
+# https://leetcode.com/explore/interview/card/top-interview-questions-medium/103/array-and-strings/776/
+
+class Solution:
+    def threeSum(self, nums):
+        result = []
+        nums.sort()
+        for i in range(len(nums) - 2):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            left = i+1
+            right = len(nums) - 1
+            while left < right:
+                total = nums[i] + nums[left] + nums[right]
+                if total < 0:
+                    left+=1
+                elif total > 0:
+                    right-=1
+                else:
+                    result.append([nums[i], nums[left], nums[right]])
+                    while left < right and nums[left]==nums[left+1]:
+                        left+=1
+                    while left < right and nums[right]==nums[right-1]:
+                        right-=1
+                    left+=1
+                    right-=1
+        return result
